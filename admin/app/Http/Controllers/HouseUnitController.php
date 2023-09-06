3203af971190fd51d96ba5255fcc244f84d99676
ari3skin <ryankinotikathurima@gmail.com>
2023-09-06 08:58:48 +0300

    doing backups for device reset

---
+++ admin/app/Http/Controllers/HouseUnitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HouseUnit;
+use Illuminate\Http\Request;
+
+class HouseUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HouseUnit  $houseUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HouseUnit $houseUnit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HouseUnit  $houseUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HouseUnit $houseUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HouseUnit  $houseUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HouseUnit $houseUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HouseUnit  $houseUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HouseUnit $houseUnit)
+    {
+        //
+    }
+}
---
+++ admin/app/Http/Controllers/HouseUnitOccupancyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HouseUnitOccupancy;
+use Illuminate\Http\Request;
+
+class HouseUnitOccupancyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HouseUnitOccupancy  $houseUnitOccupancy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HouseUnitOccupancy $houseUnitOccupancy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HouseUnitOccupancy  $houseUnitOccupancy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HouseUnitOccupancy $houseUnitOccupancy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HouseUnitOccupancy  $houseUnitOccupancy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HouseUnitOccupancy $houseUnitOccupancy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HouseUnitOccupancy  $houseUnitOccupancy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HouseUnitOccupancy $houseUnitOccupancy)
+    {
+        //
+    }
+}
